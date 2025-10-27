e877788c1ca930d32e0ebb8b386c76edef40f193
Atul Pratap Singh <atulthakur7795@gmail.com>
2025-10-27 19:48:55 +0530

    Update grade calculator

--- 1_A_Practice_Questions/Grade_calculator.py
+++ 1_A_Practice_Questions/Grade_calculator.py
@@ -1,15 +1,17 @@
 n=float(input("Enter your marks/persantage: "))
-if(n>=90 and n<=100):
-    print("A Grade")
+if n>100:
+    print("Invalid Input \n Please enter Valid input")
+elif(n>=90 and n<=100):
+    print("A+ Grade")
 elif(n>=80 and n<90):
-    print("B Grade")
+    print("A Grade")
 elif(n>=70 and n<80):
-    print("C Grade")
+    print("B+ Grade")
 elif(n>=60 and n<70):
-    print("D Grade")
+    print("B Grade")
 elif(n>=50 and n<60):
     print("Average")
 elif(n>=40 and n<50):
     print("Poor")
 else:
-    print("Fail \nNext Try Good Luck")
+    print("Fail \n Next Try Good Luck")
