aa30442c0dcf4af52010ad6920bbd97b07d08eb9
Atul Pratap Singh <atulthakur7795@gmail.com>
2025-11-22 14:09:27 +0530

    sdh

--- 1_A_Practice_Questions/factorial.py
+++ 1_A_Practice_Questions/factorial.py
@@ -1,11 +1,11 @@
 n=int(input("Enter a no.="))
-f=1
-for i in range(1,n+1):
-    f *= i
-print(f)
 # f=1
-# i=1
-# while i<=n:
-#     f*=i
-#     i+=1
+# for i in range(1,n+1):
+#     f *= i
 # print(f)
+f=1
+i=1
+while i<=n:
+    f*=i
+    i+=1
+print(f)
